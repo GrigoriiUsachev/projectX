d242594d6c0cbf7695da7526c1e3fef0da1265fa
GrigoriiUsachev <usachevgrigoriy@gmail.com>
2026-01-20 00:42:59 -0500

    fix: add Config class for shared configuration

---
+++ shared/config/config.py
@@ -0,0 +1,6 @@
+class Config:
+    BASE_URL = "https://www.saucedemo.com/"
+    STANDARD_USER = "standard_user"
+    PASSWORD = "secret_sauce"
+
+    PETSTORE_BASE_URL = "https://petstore.swagger.io/v2"
